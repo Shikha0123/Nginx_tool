409fa7087772db71681fbf5642cce01dcfb6a5b1
Shikha Tripathi <140466237+Shikha0123@users.noreply.github.com>
2023-12-15 15:51:54 +0530

    Update main.tf

--- aws_assignment_5/network/main.tf
+++ aws_assignment_5/network/main.tf
@@ -102,8 +102,8 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 resource "aws_vpc_peering_connection" "vpc_peering" {
-  vpc_id          = "vpc-08aaf997c798d67a0"
-  peer_vpc_id     = aws_vpc.tem_vpc.id
+  peer_vpc_id   = "vpc-09ce61e6b184cab91"
+  vpc_id        = "vpc-08aaf997c798d67a0"
   peer_region     = "us-east-1"
   auto_accept     = true
 
